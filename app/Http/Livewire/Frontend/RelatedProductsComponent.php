50e6a6455298e79f4759ad21cd27fc31caa4134c
Baseel <basilinreallife@gmail.com>
2023-08-20 16:06:42 +0300

    Inital commit

---
+++ app/Http/Livewire/Frontend/RelatedProductsComponent.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Livewire\Frontend;
+
+use App\Models\Product;
+use Gloudemans\Shoppingcart\Facades\Cart;
+use Livewire\Component;
+use RealRashid\SweetAlert\Facades\Alert;
+
+class RelatedProductsComponent extends Component
+{
+    public $relatedProducts;
+
+    public function mount($relatedProducts)
+    {
+        $this->relatedProducts = $relatedProducts;
+    }
+
+    public function addToCart($id)
+    {
+        $product = Product::whereId($id)->Active()->HasQuantity()->ActiveCategory()->firstOrFail();
+        $duplicates = Cart::instance('default')->search(function ($cartItem, $rowId) use ($product) {
+            return $cartItem->id === $product->id;
+        });
+        if ($duplicates->isNotEmpty()) {
+            $this->alert('error', 'Product already exist!');
+        } else {
+            Cart::instance('default')->add($product->id, $product->name, 1, $product->price)->associate(Product::class);
+            $this->emit('updateCart');
+            $this->alert('success', 'Product added in your cart successfully.');
+        }
+    }
+
+    public function addToWishList($id)
+    {
+        $product = Product::whereId($id)->Active()->HasQuantity()->ActiveCategory()->firstOrFail();
+        $duplicates = Cart::instance('wishlist')->search(function ($cartItem, $rowId) use ($product) {
+            return $cartItem->id === $product->id;
+        });
+        if ($duplicates->isNotEmpty()) {
+            $this->alert('error', 'Product already exist!');
+        } else {
+            Cart::instance('wishlist')->add($product->id, $product->name, 1, $product->price)->associate(Product::class);
+            $this->emit('updateCart');
+            $this->alert('success', 'Product added in your wishlist cart successfully.');
+        }
+    }
+
+    public function alert($type, $message)
+    {
+        $this->dispatchBrowserEvent('alert', ['type' => $type, 'message' => $message]);
+    }
+    public function render()
+    {
+        return view('livewire.frontend.related-products-component');
+    }
+}
